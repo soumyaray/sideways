9473c5d843e4b8e51c3eed317229731b9393cc3c
Soumya Ray <soumya.ray@gmail.com>
2026-02-06 18:19:20 +0800

    Bump version to 0.5.1

--- worktrees.sh
+++ worktrees.sh
@@ -5,7 +5,7 @@
 #   # Sideways - git worktree helper
 #   source ~/path/to/worktrees.sh
 
-SW_VERSION="0.5.0"
+SW_VERSION="0.5.1"
 
 # =============================================================================
 # MODEL LAYER - Core business logic and state queries
